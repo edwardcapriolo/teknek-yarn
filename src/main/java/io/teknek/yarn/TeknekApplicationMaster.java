79e595186e0b71f2f8ef24d0fe7e3846b9d78ab7
Edward Capriolo <edlinuxguru@gmail.com>
2014-01-18 16:51:16 -0500

    Something that works

--- src/main/java/io/teknek/yarn/TeknekApplicationMaster.java
+++ src/main/java/io/teknek/yarn/TeknekApplicationMaster.java
@@ -1,7 +1,5 @@
 package io.teknek.yarn;
 
-import io.teknek.daemon.TeknekDaemon;
-
 import java.util.Collections;
 
 import org.apache.hadoop.conf.Configuration;
@@ -9,7 +7,6 @@ import org.apache.hadoop.yarn.api.ApplicationConstants;
 import org.apache.hadoop.yarn.api.protocolrecords.AllocateResponse;
 import org.apache.hadoop.yarn.api.records.Container;
 import org.apache.hadoop.yarn.api.records.ContainerLaunchContext;
-import org.apache.hadoop.yarn.api.records.ContainerStatus;
 import org.apache.hadoop.yarn.api.records.FinalApplicationStatus;
 import org.apache.hadoop.yarn.api.records.Priority;
 import org.apache.hadoop.yarn.api.records.Resource;
@@ -27,7 +24,10 @@ public class TeknekApplicationMaster {
 
     //String jarPath = "/home/edward/Documents/java/teknek-yarn/target/teknek-yarn-0.0.1-SNAPSHOT-jar-with-dependencies.jar" ;
     String jarPath = "file:///tmp/teknek-yarn-0.0.1-SNAPSHOT-jar-with-dependencies.jar" ;
-    
+    int n = 1;
+    if (args.length>0){
+      n = Integer.parseInt(args[0]);
+    }
     Configuration conf = new YarnConfiguration();
 
     AMRMClient<ContainerRequest> rmClient = AMRMClient.createAMRMClient();
@@ -49,42 +49,33 @@ public class TeknekApplicationMaster {
 
     // Resource requirements for worker containers
     Resource capability = Records.newRecord(Resource.class);
-    capability.setMemory(4096);
+    capability.setMemory(2048);
     capability.setVirtualCores(1);
 
-    ContainerRequest containerAsk = new ContainerRequest(capability, null, null, priority);
-    rmClient.addContainerRequest(containerAsk);
-    AllocateResponse response = rmClient.allocate(0);
+    /* for (int i=0; i< n;i++){
+      ContainerRequest containerAsk = new ContainerRequest(capability, null, null, priority);
+      rmClient.addContainerRequest(containerAsk);
+    } */
     
-    
-    while(response.getAllocatedContainers().size()==0){
-      System.out.println("waiting for container");
-      response = rmClient.allocate(0);
-      Thread.sleep(1000);
-    }
-    System.out.println("Have containers "+response.getAllocatedContainers().size());
-    
-    for (Container container : response.getAllocatedContainers()) {
-      ContainerLaunchContext ctx = Records.newRecord(ContainerLaunchContext.class);
-      
-      /*
-      ctx.setCommands(Collections.singletonList("$JAVA_HOME/bin/java" + " -cp " + jarPath + " "
-              + TeknekYarnStarter.class.getName() + " 1>"
-              + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout" + " 2>"
-              + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr"
-              )); 
-      */
-      ctx.setCommands(Collections.singletonList("/usr/java/jdk1.7.0_45/bin/java -cp file:///tmp/teknek-yarn-0.0.1-SNAPSHOT-jar-with-dependencies.jar io.teknek.yarn.TeknekYarnStarter")); 
-      
-      
-      System.out.println(ctx.getCommands());
-      System.out.println(container); 
-      nmClient.startContainer(container, ctx);
+    int allocated = 0;
+    while (allocated < n) {
+      ContainerRequest containerAsk = new ContainerRequest(capability, null, null, priority);
+      rmClient.addContainerRequest(containerAsk);
+      AllocateResponse response = rmClient.allocate(0);
+      for (Container container : response.getAllocatedContainers()) {
 
-      
-      System.out.println("after startContainer "); 
+        allocated++;
+        ContainerLaunchContext ctx = Records.newRecord(ContainerLaunchContext.class);
+        ctx.setCommands(Collections.singletonList("$JAVA_HOME/bin/java" + " -cp " + jarPath + " "
+                + TeknekYarnStarter.class.getName() + " 1>"
+                + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout" + " 2>"
+                + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr"));
+        nmClient.startContainer(container, ctx);
+        Thread.sleep(200);
+        System.out.println("after startContainer ");
+      }
     }
-
+    System.out.println("Should be alll alocated");
     Thread.sleep(1000000);
     rmClient.unregisterApplicationMaster(FinalApplicationStatus.SUCCEEDED, "", "");
   }
--- src/main/java/io/teknek/yarn/TeknekYarn.java
+++ src/main/java/io/teknek/yarn/TeknekYarn.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
@@ -37,32 +38,65 @@ import org.apache.hadoop.yarn.util.Records;
  */
 public class TeknekYarn {
 
+  public static final String TEKNEK_APP_NAME = "teknek-yarn-app";
   
-  public static void main (String [] args) throws IllegalArgumentException, IOException, YarnException{
-    //String jarPath = TeknekApplicationMaster.class.getProtectionDomain().getCodeSource().getLocation().getFile();
-    String pth = "file:///tmp/teknek-yarn-0.0.1-SNAPSHOT-jar-with-dependencies.jar" ;
-
-    YarnConfiguration conf = new YarnConfiguration();
+  /**
+   * 
+   * @throws IOException 
+   * @throws YarnException
+   * @return if tekenek running return application report else return null
+   */
+  public static ApplicationReport maybeFindReportForTeknek(YarnClient yarnClient, String appName) 
+          throws YarnException, IOException{
+    List<ApplicationReport> currentApplications = yarnClient.getApplications();
+    for (ApplicationReport report : currentApplications){
+      if (report.getName().equals(TEKNEK_APP_NAME)
+              && (report.getYarnApplicationState() == YarnApplicationState.RUNNING || report
+                      .getYarnApplicationState() == YarnApplicationState.SUBMITTED)) {
+        return report;
+      }
+    }
+    return null;
+  }
+  
+  public static YarnClient createAndStartYarn(YarnConfiguration conf){
     YarnClient yarnClient = YarnClient.createYarnClient();
     yarnClient.init(conf);
     yarnClient.start();
+    return yarnClient;
+  }
+  
+  public static void main (String [] args) throws IllegalArgumentException, IOException, YarnException, InterruptedException{
     
+    YarnConfiguration conf = new YarnConfiguration();
+    YarnClient yarnClient = createAndStartYarn(conf);
+    ApplicationReport tekRunning = maybeFindReportForTeknek(yarnClient, TEKNEK_APP_NAME);
+    if (tekRunning != null){
+      System.out.println("Teknek already running or submitted.");
+      System.out.println("Tracking url:" + tekRunning.getOriginalTrackingUrl());
+      return;
+    }
+    
+    int n = 2;
+    if (args.length > 0) {
+      n = Integer.parseInt(args[0]);
+    }
+    //String jarPath = TeknekApplicationMaster.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+    String pth = "file:///tmp/teknek-yarn-0.0.1-SNAPSHOT-jar-with-dependencies.jar" ;
     Path jarPath = new Path(pth);
     jarPath = FileSystem.get(conf).makeQualified(jarPath);
-    
+
     YarnClientApplication app = yarnClient.createApplication();
     ContainerLaunchContext amContainer = Records.newRecord(ContainerLaunchContext.class);
     amContainer.setCommands(Collections.singletonList("$JAVA_HOME/bin/java" + " -Xmx256M"
-            + " io.teknek.yarn.TeknekApplicationMaster 1> " 
+            + " io.teknek.yarn.TeknekApplicationMaster "+n+" 1> " 
             + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout" + " 2> "
             + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr"
             ));
-
+    
     LocalResource appMasterJar = Records.newRecord(LocalResource.class);
     setupAppMasterJar(conf, jarPath, appMasterJar);
-    amContainer.setLocalResources(
-        //Collections.singletonMap("simpleapp.jar", appMasterJar));
-            Collections.singletonMap("teknek-yarn.jar", appMasterJar));
+    amContainer.setLocalResources( Collections.singletonMap("teknek-yarn.jar", appMasterJar));
     
     Map<String, String> appMasterEnv = new HashMap<String, String>();
     setupAppMasterEnv(conf, appMasterEnv);
@@ -73,7 +107,7 @@ public class TeknekYarn {
     capability.setVirtualCores(1);
     
     ApplicationSubmissionContext appContext = app.getApplicationSubmissionContext();
-    appContext.setApplicationName("simple-yarn-app"); // application name
+    appContext.setApplicationName(TEKNEK_APP_NAME); // application name
     appContext.setAMContainerSpec(amContainer);
     appContext.setResource(capability);
     appContext.setQueue("default"); // queue
@@ -81,24 +115,19 @@ public class TeknekYarn {
     ApplicationId appId = appContext.getApplicationId();
     System.out.println("Submitting application " + appId);
     yarnClient.submitApplication(appContext);
+    Thread.sleep(5000);
+    printApplicationStatistics(yarnClient, appId);
 
+  }
+  
+  private static void printApplicationStatistics(YarnClient yarnClient, ApplicationId appId) throws YarnException, IOException{
     ApplicationReport appReport = yarnClient.getApplicationReport(appId);
     YarnApplicationState appState = appReport.getYarnApplicationState();
-    while (appState != YarnApplicationState.FINISHED && appState != YarnApplicationState.KILLED
-            && appState != YarnApplicationState.FAILED) {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      appReport = yarnClient.getApplicationReport(appId);
-      System.out.println(appReport.getApplicationType());
-      System.out.println(appReport.getDiagnostics());
-      System.out.println(appReport.getTrackingUrl());
-      appState = appReport.getYarnApplicationState();
-      
-    }
-
+    appReport = yarnClient.getApplicationReport(appId);
+    System.out.println("State:" + appState);
+    System.out.println("Type:" + appReport.getApplicationType());
+    System.out.println("Diagnostics:" + appReport.getDiagnostics());
+    System.out.println("Url:" + appReport.getTrackingUrl());
   }
   
   private static void setupAppMasterJar(Configuration conf, Path jarPath, LocalResource appMasterJar) throws IOException {
@@ -120,3 +149,22 @@ public class TeknekYarn {
             + File.separator + "*");
   }
 }
+
+/*
+ApplicationReport appReport = yarnClient.getApplicationReport(appId);
+YarnApplicationState appState = appReport.getYarnApplicationState();
+while (appState != YarnApplicationState.FINISHED && appState != YarnApplicationState.KILLED
+        && appState != YarnApplicationState.FAILED) {
+  try {
+    Thread.sleep(5000);
+  } catch (InterruptedException e) {
+    e.printStackTrace();
+  }
+  appReport = yarnClient.getApplicationReport(appId);
+  System.out.println(appReport.getApplicationType());
+  System.out.println(appReport.getDiagnostics());
+  System.out.println(appReport.getTrackingUrl());
+  appState = appReport.getYarnApplicationState();
+  
+}
+*/
